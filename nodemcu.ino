44166a20e4c5cf9bcab03d1a4b9b94fb37b2f0aa
thor <sylvain.muller90@gmail.com>
2018-10-02 13:15:12 +0200

    Demo ready

--- nodemcu.ino
+++ nodemcu.ino
@@ -6,11 +6,12 @@
 #include <AES.h> //https://github.com/spaniakos/AES
 #include <Base64.h> //https://github.com/adamvr/arduino-base64
 #include <ArduinoJson.h> //https://github.com/bblanchon/ArduinoJson
+#include <ESP8266TrueRandom.h> //https://github.com/marvinroger/ESP8266TrueRandom
 
 #define DEBUG true
 
-const char *ssid = "......";
-const char *password = ".....";
+const char *ssid = "...";
+const char *password = "...";
 
 int sensor = -1;
 
@@ -18,16 +19,18 @@ ESP8266WebServer server(80);
 AES aes;
 
 void getWind() {
+  digitalWrite(2, LOW);
   // Rest Client
   RestClient client = RestClient("192.168.192.29", 443, 1);
   String response = "";
-  client.setHeader("authorization: Basic NTQzMjEw");
+  client.setHeader("authorization: Basic NTQzMjEwOjU0MzIxMDk4NzY1NDMyMTA=");
   String bearer = server.header("Authorization");
   String accessToken = "token: " + bearer.substring(7, bearer.length());
   char token[accessToken.length() + 1];
   accessToken.toCharArray(token, accessToken.length() + 1);
   client.setHeader(token);
   int statusCode = client.get("/introspect", &response);
+  Serial.println(response);
 
   // Parsing json response
   char json[response.length() + 1];
@@ -35,27 +38,30 @@ void getWind() {
   StaticJsonBuffer<100> jsonBuffer; // 50 for single mode, and 60 for cbc with IV
   JsonObject& input = jsonBuffer.parseObject(json);
   bool active = input["active"];
-  Serial.println(response);
   switch(statusCode) {
     case 200:
       if (active) {
         String key = input["key"];
-        char test[key.length() + 1];
-        key.toCharArray(test, key.length() + 1);
-        unsigned long long int my_iv = 36753562;
+        char aesKey[key.length() + 1];
+        key.toCharArray(aesKey, key.length() + 1);
         //process message here
-        sensor = digitalRead(4);
-        byte plain[] = "La force du vent est de 117 sur l'échelle de Beaufort";
+
+        char randomNum[16];
+        ESP8266TrueRandom.memfill(randomNum, 16);
+        String randomString = randomNum;
+        String response;
+        if (sensor) {
+          response = "Detected movement";
+        } else {
+          response = "Undetected movement";
+        }
+        String plaintext = randomString + response;
+        byte plain[plaintext.length() + 1];
+        plaintext.getBytes(plain, plaintext.length() + 1);
         int plainLength = sizeof(plain)-1;  // don't count the trailing /0 of the string !
-        int padedLength = plainLength + N_BLOCK - plainLength % N_BLOCK;
-        aes.iv_inc();
-        byte iv [N_BLOCK] ;
-        byte plain_p[padedLength];
-        byte cipher [padedLength] ;
-        byte check [padedLength] ;
-        aes.set_IV(my_iv);
-        aes.get_IV(iv);
-        aes.do_aes_encrypt(plain,plainLength,cipher,(byte *)test,256,iv);
+        int padedLength = plainLength + N_BLOCK - plainLength % N_BLOCK;;
+        byte cipher[padedLength] ;
+        aes.do_aes_encrypt(plain,plainLength,cipher,(byte *)aesKey,256);
 
         // base 64 encoding
         int inputLen = sizeof(cipher);
@@ -66,23 +72,24 @@ void getWind() {
 
         StaticJsonBuffer<500> jsonBuffer;
         JsonObject& root = jsonBuffer.createObject();
-        root["message"] = encoded;
-        root["iv"] = 36753562;
+        root["encoded"] = encoded;
+        root["length"] = plainLength;
         String output;
         root.printTo(output);
-        
+
         server.send(statusCode, "application/json", output);
-      } 
-      
+      }
+
     default:
         StaticJsonBuffer<200> jsonBuffer;
         JsonObject& root = jsonBuffer.createObject();
         root["error"] = "access denied";
-        String output;
+        String output;        sensor = digitalRead(4);
         root.printTo(output);
         server.send(401, "application/json", output);
 
   }
+  digitalWrite(2, HIGH);
 }
 
 void handleNotFound() {
@@ -96,7 +103,8 @@ void setup() {
   #endif
 
   pinMode(4, INPUT);
-  
+  pinMode(2, OUTPUT);
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("");
@@ -117,13 +125,15 @@ void setup() {
     Serial.println("MDNS responder started");
   }
 
-  server.on("/weather", HTTP_GET, getWind);
+  server.on("/wind/direction", HTTP_GET, getWind);
   server.onNotFound(handleNotFound);
   server.begin();
-  
+
   Serial.println("HTTP server started");
+  digitalWrite(2, HIGH);
 }
 
 void loop() {
+  sensor = digitalRead(4);
   server.handleClient();
 }
